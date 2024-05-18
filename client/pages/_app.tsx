4cf4106d3eb9a515d4adf8511af23074f8ca41ed
Yuki Okuno <y.okuno.btm@gmail.com>
2024-05-18 20:36:17 +0900

    feat: 🎸 ヘッダーのNavbar UIを追加

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
+
+import { Navbar } from "@/components";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  );
 }

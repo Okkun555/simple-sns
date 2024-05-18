1b5ed6438507c1e4661d9489835d3c703a0f9e35
Yuki Okuno <y.okuno.btm@gmail.com>
2024-05-18 15:15:03 +0900

    feat: 🎸 ユーザー登録APIを実装

--- api/server.js
+++ api/server.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt")
+
 const app = express();
+app.use(express.json())
 const PORT = 8010;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello world!!</h1>")
+const prisma = new PrismaClient();
+
+// 新規ユーザーの登録
+app.post("/api/auth/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  })
+
+  return res.json(user);
 })
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
